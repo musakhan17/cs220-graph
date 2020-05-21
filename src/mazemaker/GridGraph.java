e13f314a276bd68131268bf837044affb31a6a6b
Jaime Spacco <jspacco@knox.edu>
2020-05-21 14:39:01 -0500

    grid graph code

---
+++ src/mazemaker/GridGraph.java
@@ -0,0 +1,172 @@
+package mazemaker;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+import graph.IGraph;
+import graph.INode;
+import graph.NodeVisitor;
+import graph.impl.Graph;
+
+/**
+ * Pretty cool class for creating "grid graphs", which are graphs with the nodes
+ * arranged on a grid of rows and columns. Perfomring a DFS on this graph creates
+ * a maze.
+ *
+ * @author jspacco
+ *
+ */
+public class GridGraph
+{
+    public static IGraph makeGridGraph(int rows, int cols) {
+        IGraph graph = new Graph();
+        // Each node is named something like r2c1, which means "row 2, column 1".
+        // Nodes have edges to other nodes that are to their left or right, and
+        // above or below.
+        for (int r = 0; r < rows; r++){
+            for (int c=0; c < cols; c++){
+                if (r > 0) {
+                    INode current = graph.getOrCreateNode(String.format("r%dc%d", r, c));
+                    INode other = graph.getOrCreateNode(String.format("r%dc%d", r-1, c));
+                    current.addUndirectedEdgeToNode(other, 1);
+                }
+                if (r < rows-1) {
+                    INode current = graph.getOrCreateNode(String.format("r%dc%d", r, c));
+                    INode other = graph.getOrCreateNode(String.format("r%dc%d", r+1, c));
+                    current.addUndirectedEdgeToNode(other, 1);
+                }
+                if (c > 0) {
+                    INode current = graph.getOrCreateNode(String.format("r%dc%d", r, c));
+                    INode other = graph.getOrCreateNode(String.format("r%dc%d", r, c-1));
+                    current.addUndirectedEdgeToNode(other, 1);
+                }
+                if (c < cols-1) {
+                    INode current = graph.getOrCreateNode(String.format("r%dc%d", r, c));
+                    INode other = graph.getOrCreateNode(String.format("r%dc%d", r, c+1));
+                    current.addUndirectedEdgeToNode(other, 1);
+                }
+            }
+        }
+        return graph;
+    }
+
+    public static IGraph createMaze(IGraph g, String start){
+        // g should be a GridGraph
+        IGraph result = new Graph();
+
+        g.depthFirstSearch(start, new NodeVisitor() {
+            // stack of nodes we've already seen in the order in which we've seen them
+            Stack<INode> nodes = new Stack<INode>();
+            @Override
+            public void visit(INode node) {
+                // Go through the stack of nodes we've already seen,
+                // and connect to it.
+                for (INode n : nodes){
+                    if (node.hasEdge(n)){
+                        INode src = result.getOrCreateNode(n.getName());
+                        INode dst = result.getOrCreateNode(node.getName());
+                        src.addUndirectedEdgeToNode(dst, 1);
+                        break;
+                    }
+                }
+                nodes.push(node);
+            }
+        });
+        return result;
+    }
+
+    public static String generateGraphVizGridGraph(IGraph g, int numRows, int numCols, String name, String start){
+        StringBuilder result = new StringBuilder();
+        result.append(String.format("graph %s {\n", name));
+
+        for (int r=0; r<numRows; r++){
+            List<String> list = new LinkedList<String>();
+            for (int c=0; c<numCols; c++){
+                String label = String.format("r%dc%d", r, c);
+                list.add(label);
+                //System.out.printf("%s [\n\tlabel = %s\npos = \"%d,%d\" ];\n", label, label, r, c);
+                //rank += label + ", ";
+            }
+            String rank = String.join(", ", list);
+            // remove last comma
+            result.append(String.format("{ rank=same; %s }\n", rank));
+        }
+
+        result.append("}");
+        return result.toString();
+    }
+
+    public static void writeToFile(String filename, String text){
+        try {
+            FileWriter writer = new FileWriter(filename);
+            writer.write(text);
+            writer.flush();
+            writer.close();
+        } catch (IOException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args){
+        int rows = 5;
+        int cols = 5;
+        
+        IGraph g = makeGridGraph(rows, cols);
+
+        // r0c0 is always the top-left node
+        // but we could make the start any node on the edges
+        IGraph maze = createMaze(g, "r0c0");
+
+        String graphviz = generateGraphVizGridGraph(maze, rows, cols, "maze1", "r0c0");
+
+        writeToFile("files/maze.dot", graphviz);
+    }
+
+    public static void main2(String[] args){
+        int rows = 5;
+        int cols = 5;
+
+        IGraph g = makeGridGraph(rows, cols);
+
+        // This code generates a maze with the given number of rows and cols
+        // using DFS, and prints it out in Graphviz format.
+        System.out.printf("graph gr {\n");
+
+
+        g.depthFirstSearch("r0c0", new NodeVisitor() {
+            Stack<INode> nodes = new Stack<INode>();
+            @Override
+            public void visit(INode node) {
+                // Go through the stack of nodes we've already seen,
+                // and connect to it.
+                for (INode n : nodes){
+                    if (node.hasEdge(n)){
+                        System.out.printf("%s -- %s;\n", n.getName(), node.getName());
+                        break;
+                    }
+                }
+                nodes.push(node);
+            }
+        });
+
+        // This creates a bunch of "rank=same" directives that tell graphviz to lay out
+        // the graph a certain way.
+        for (int r=0; r<rows; r++){
+            String rank = "";
+            for (int c=0; c<cols; c++){
+                String label = String.format("r%dc%d", r, c);
+                //System.out.printf("%s [\n\tlabel = %s\npos = \"%d,%d\" ];\n", label, label, r, c);
+                rank += label + ", ";
+            }
+            // remove last comma
+            rank = rank.substring(0, rank.length()-2);
+            System.out.printf("{ rank=same; %s }\n", rank);
+        }
+
+        System.out.printf("}\n");
+    }
+
+}
